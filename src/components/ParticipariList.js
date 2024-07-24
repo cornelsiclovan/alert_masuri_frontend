450e3ab97d01193b96a8a132f82d7615a1d38f20
cornel <cornel.siclovan@gmail.com>
2024-07-24 10:24:03 +0300

    participari semestru

--- src/components/ParticipariList.js
+++ src/components/ParticipariList.js
@@ -1,27 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import classes from "./SolutiiList.module.css";
 
 const ParticipariList = ({ participari }) => {
-
-
   const [anPart, setAnPart] = useState("2024");
   const [numeProcuror, setNumeProcuror] = useState("");
   const [lunaPart, setLunaPart] = useState("");
+  const [semestru, setSemestru] = useState(false);
+  const [partSemestru, setPartSemestru] = useState([]);
 
-  let year = (new Date()).getFullYear();
-  let yearArray = []
-  for(let j = 0; j < 2; j++) {
+  let year = new Date().getFullYear();
+  let yearArray = [];
+  for (let j = 0; j < 2; j++) {
     yearArray.push(year - j);
   }
 
   let procuroriList = [];
 
   participari.forEach((element) => {
-
-    if(procuroriList.includes(element.numeProcuror)) {
-
-    }else  if(+element.an === +anPart){
-
+    if (procuroriList.includes(element.numeProcuror)) {
+    } else if (+element.an === +anPart) {
       procuroriList.push(element.numeProcuror);
     }
   });
@@ -40,7 +37,7 @@ const ParticipariList = ({ participari }) => {
 
   partFiltrate = partFiltrate.filter((part) => {
     if (anPart === "") {
-      setNumeProcuror("")
+      setNumeProcuror("");
       return true;
     } else {
       return part.an.toString().includes(anPart);
@@ -48,7 +45,6 @@ const ParticipariList = ({ participari }) => {
   });
 
   partFiltrate = partFiltrate.filter((part) => {
-    
     if (+lunaPart === 0) {
       return true;
     } else {
@@ -56,20 +52,69 @@ const ParticipariList = ({ participari }) => {
     }
   });
 
-
-  
   const onChangeNumeProcurorInput = (event) => {
     setNumeProcuror(event.target.value);
   };
 
   const onChangeAnInput = (event) => {
-  
     setAnPart(event.target.value);
   };
 
   const onChangeLunaInput = (event) => {
- 
     setLunaPart(event.target.value);
+  };
+
+  const checkSemestru = () => {
+    setSemestru(!semestru);
+
+    console.log(semestru);
+    if (semestru) {
+      let sem = [];
+      let mapSem = [];
+      partFiltrate.map((part) => {
+        let index = 0;
+        if (sem.length === 0 && +part.luna <= 6) {
+          mapSem.push(part.numeProcuror);
+          sem.push({ ...part });
+          index++;
+        } else if( +part.luna <= 6) {
+          if (mapSem.includes(part.numeProcuror)) {
+            const myIndex = mapSem.indexOf(part.numeProcuror);
+            console.log(part.nr_part_sed);
+
+            if (!sem[myIndex].nr_part_sed) {
+              sem[myIndex].nr_part_sed = 0;
+              sem[myIndex].nr_part_cauze = 0;
+              sem[myIndex].nr_hot_vf = 0;
+              sem[myIndex].nr_part_sed_copil = 0;
+              sem[myIndex].nr_part_copil = 0;
+              sem[myIndex].nr_hot_vf_copil = 0;
+              
+            }
+
+            
+
+            sem[myIndex].nr_part_sed = +sem[myIndex].nr_part_sed + +part.nr_part_sed;
+            sem[myIndex].nr_part_cauze =
+              +sem[myIndex].nr_part_cauze + +part.nr_part_cauze;
+            sem[myIndex].nr_hot_vf = +sem[myIndex].nr_hot_vf + +part.nr_hot_vf;
+            sem[myIndex].nr_part_sed_copil =
+              +sem[myIndex].nr_part_sed_copil + +part.nr_part_sed_copil;
+            sem[myIndex].nr_part_copil =
+              +sem[myIndex].nr_part_copil + +part.nr_part_copil;
+            sem[myIndex].nr_hot_vf_copil =
+              +sem[myIndex].nr_hot_vf_copil + +part.nr_hot_vf_copil;
+          } else {
+            console.log(part);
+            sem.push({ ...part });
+            mapSem.push(part.numeProcuror);
+            index++;
+          }
+        }
+      });
+
+      setPartSemestru([...sem]);
+    }
   };
 
   let tot1 = 0;
@@ -77,27 +122,26 @@ const ParticipariList = ({ participari }) => {
   let tot3 = 0;
   let tot4 = 0;
   let tot5 = 0;
-  let tot6 = 0
+  let tot6 = 0;
   let i = 0;
   return (
     <>
-    
       <div className={classes.sort}>
-      <select
+        <select
           style={{
             height: "30px",
             marginTop: "20px",
             marginLeft: "-100px",
             width: "150px",
           }}
-
           defaultValue={2024}
           type="text"
           placeholder="an"
           onChange={onChangeAnInput}
         >
-          { yearArray.map(year => <option>{year}</option>)}
-
+          {yearArray.map((year) => (
+            <option>{year}</option>
+          ))}
         </select>
 
         <select
@@ -112,8 +156,9 @@ const ParticipariList = ({ participari }) => {
           onChange={onChangeNumeProcurorInput}
         >
           <option value="">Alege un procuror</option>
-          {procuroriList.map(procuror => <option value={procuror}>{procuror}</option>)}
-
+          {procuroriList.map((procuror) => (
+            <option value={procuror}>{procuror}</option>
+          ))}
         </select>
 
         <select
@@ -128,43 +173,29 @@ const ParticipariList = ({ participari }) => {
           onChange={onChangeLunaInput}
         >
           <option value="0">Alege luna</option>
-          <option value="1">
-            Ianuarie
-          </option>
-          <option value="2">
-            Februarie
-          </option>
-          <option value="3">
-            Martie
-          </option>
-          <option value="4">
-            Aprilie
-          </option>
-          <option value="5">
-            Mai
-          </option>
-          <option value="6">
-            Iunie
-          </option>
-          <option value="7">
-            Iulie
-          </option>
-          <option value="8">
-            August
-          </option>
-          <option value="9">
-            Septembrie
-          </option>
-          <option value="10">
-            Octombrie
-          </option>
-          <option value="11">
-            Noiembrie
-          </option>
-          <option value="12">
-            Decembrie
-          </option>
+          <option value="1">Ianuarie</option>
+          <option value="2">Februarie</option>
+          <option value="3">Martie</option>
+          <option value="4">Aprilie</option>
+          <option value="5">Mai</option>
+          <option value="6">Iunie</option>
+          <option value="7">Iulie</option>
+          <option value="8">August</option>
+          <option value="9">Septembrie</option>
+          <option value="10">Octombrie</option>
+          <option value="11">Noiembrie</option>
+          <option value="12">Decembrie</option>
         </select>
+        <div>
+          <input
+            checked={semestru}
+            type="checkbox"
+            id="toate"
+            value="toate"
+            onChange={checkSemestru}
+          ></input>
+          <label>Semestru</label>
+        </div>
       </div>
 
       <div className={classes.table}>
@@ -186,31 +217,56 @@ const ParticipariList = ({ participari }) => {
             NR. HOT. VERIFICATE <br /> COPIL
           </div>
           <div className={classes.td}>AN</div>
-          <div className={classes.td}>LUNA</div>
+        {!semestru &&  <div className={classes.td}>LUNA</div>}
         </div>
-        {partFiltrate.map((part) => {
-          i = i + 1;
-          tot1 = tot1 + part.nr_part_sed;
-          tot2 = tot2 + part.nr_part_cauze;
-          tot3 = tot3 + part.nr_hot_vf;
-          tot4 = tot4 + part.nr_part_sed_copil;
-          tot5 = tot5 + part.nr_part_copil;
-          tot6 = tot6 + part.nr_hot_vf_copil;
-          return (
-            <div className={classes.tr} key={i}>
-              <div className={classes.td}>{i}</div>
-              <div className={classes.td}>{part.numeProcuror}</div>
-              <div className={classes.td}>{part.nr_part_sed}</div>
-              <div className={classes.td}>{part.nr_part_cauze}</div>
-              <div className={classes.td}>{part.nr_hot_vf}</div>
-              <div className={classes.td}>{part.nr_part_sed_copil}</div>
-              <div className={classes.td}>{part.nr_part_copil}</div>
-              <div className={classes.td}>{part.nr_hot_vf_copil}</div>
-              <div className={classes.td}>{part.an}</div>
-              <div className={classes.td}>{part.luna}</div>
-            </div>
-          );
-        })}
+        {!semestru &&
+          partFiltrate.map((part) => {
+            i = i + 1;
+            tot1 = tot1 + part.nr_part_sed;
+            tot2 = tot2 + part.nr_part_cauze;
+            tot3 = tot3 + part.nr_hot_vf;
+            tot4 = tot4 + part.nr_part_sed_copil;
+            tot5 = tot5 + part.nr_part_copil;
+            tot6 = tot6 + part.nr_hot_vf_copil;
+            return (
+              <div className={classes.tr} key={i}>
+                <div className={classes.td}>{i}</div>
+                <div className={classes.td}>{part.numeProcuror}</div>
+                <div className={classes.td}>{part.nr_part_sed}</div>
+                <div className={classes.td}>{part.nr_part_cauze}</div>
+                <div className={classes.td}>{part.nr_hot_vf}</div>
+                <div className={classes.td}>{part.nr_part_sed_copil}</div>
+                <div className={classes.td}>{part.nr_part_copil}</div>
+                <div className={classes.td}>{part.nr_hot_vf_copil}</div>
+                <div className={classes.td}>{part.an}</div>
+                <div className={classes.td}>{part.luna}</div>
+              </div>
+            );
+          })}
+        {semestru &&
+          partSemestru.map((part) => {
+            i = i + 1;
+            tot1 = tot1 + part.nr_part_sed;
+            tot2 = tot2 + part.nr_part_cauze;
+            tot3 = tot3 + part.nr_hot_vf;
+            tot4 = tot4 + part.nr_part_sed_copil;
+            tot5 = tot5 + part.nr_part_copil;
+            tot6 = tot6 + part.nr_hot_vf_copil;
+            return (
+              <div className={classes.tr} key={i}>
+                <div className={classes.td}>{i}</div>
+                <div className={classes.td}>{part.numeProcuror}</div>
+                <div className={classes.td}>{part.nr_part_sed}</div>
+                <div className={classes.td}>{part.nr_part_cauze}</div>
+                <div className={classes.td}>{part.nr_hot_vf}</div>
+                <div className={classes.td}>{part.nr_part_sed_copil}</div>
+                <div className={classes.td}>{part.nr_part_copil}</div>
+                <div className={classes.td}>{part.nr_hot_vf_copil}</div>
+                <div className={classes.td}>{part.an}</div>
+            
+              </div>
+            );
+          })}
         <div className={classes.th}>
           <div className={classes.td}>TOTAL PARTICIPARI</div>
           <div className={classes.td}></div>
@@ -221,7 +277,7 @@ const ParticipariList = ({ participari }) => {
           <div className={classes.td}>{tot5}</div>
           <div className={classes.td}>{tot6}</div>
           <div className={classes.td}></div>
-          <div className={classes.td}></div>
+          {!semestru && <div className={classes.td}></div>}
         </div>
       </div>
     </>
